8a1641e63562d5de25fa55164f4ff94071040000
sneakie <adam.fredlund@hotmail.com>
2023-01-26 19:15:31 +0100

    init

---
+++ Database.py
@@ -0,0 +1,33 @@
+import random
+
+class Question:
+    def __init__(self, prompt, answer):
+        self.prompt = prompt
+        self.answer = answer
+
+Word = [
+
+    ["Train Station", "Eki"],
+    ["Hello(day)", "Konnichiwa"],
+    ["Hello(Morning)", "Ohayo"]
+
+]
+
+apa = Word[random.randint(0, 2)]
+
+question_prompts = [
+
+    "How do you say " + apa[0] + " in Japanese?"
+]
+
+def run_test(Questions):
+
+    score = 0
+    answer = input(question_prompts)
+    if answer == apa[1]:
+        print("Rätt")
+    else:
+        print("Fel")
+
+run_test(Question)
+
