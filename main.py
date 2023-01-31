3a66c1d9b8b31fc59b71246d1c33ed4034f656d1
sneakie <adam.fredlund@hotmail.com>
2023-01-31 13:23:14 +0100

    Now does fully correct all answers and will also make sure ot never ask the same question twice.

--- main.py
+++ main.py
@@ -11,21 +11,28 @@ def run_test(Questions):
     total_question = 0
     right_question = 0
     wrong_question = 0
+    asked_question = [
+
+    ]
     while total_question < 5:
-    #for i in Word:
         apa = Word[random.randint(0, 4)]
-        question_prompts = [
+        count = asked_question.count(apa)
+        if count >= 1:
+            continue
+        else:
+            question_prompts = [
             "How do you say " + apa[0].lower() + " in Japanese?"
         ]
-        answer = input(question_prompts)
-        if answer.lower() == apa[1].lower():
-            total_question += 1
-            right_question += 1
-            print("Rätt")
-        else:
-            print("Fel")
-            wrong_question += 1
-            total_question += 1
+            answer = input(question_prompts)
+            asked_question.append(apa)
+            if answer.lower() == apa[1].lower():
+                total_question += 1
+                right_question += 1
+                print("Rätt")
+            else:
+                print("Fel")
+                total_question += 1
     print("You had " + str(right_question) + " out of " + str(total_question) + " questions correct")
+    print(asked_question)
 
 run_test(Question)
